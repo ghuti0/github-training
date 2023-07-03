febcd98f89b04769b33a0a780f8c2e7b3443383b
Polina <108152784+shtnpd@users.noreply.github.com>
2023-07-03 16:13:03 +0200

    Create task.py

---
+++ task.py
@@ -0,0 +1,10 @@
+n=0
+for x in range (245756):
+    m= []
+    for i in range(m):
+        if x%i!=0:
+            m.append(i)
+        if len(m)>0:
+            break
+    if len(m)==1:
+        print(n)
